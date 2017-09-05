18c53c52cf5aaaf543b2be110cf378ff1935b606
jamatute <jamatute@paradigmadigital.com>
2017-09-05 15:18:45 +0200

    * [feat] create root user

--- molecule/centos_systemd/tests/test_default.py
+++ molecule/centos_systemd/tests/test_default.py
@@ -20,6 +20,7 @@ def test_mongod_conf(host):
     assert f.user == 'root'
     assert f.group == 'root'
     assert oct(f.mode) == '0644'
+    # assert f.contains('keyFile:')
 
 
 def test_mongod_can_start(host):
@@ -29,8 +30,39 @@ def test_mongod_can_start(host):
 
 
 def test_mongod_has_replicaset(host):
-    out = json.loads(
+    members = json.loads(
         host.check_output(
             "echo 'rs.status().members.map(function(a) {return a.name});'" +
             "| mongo --quiet"))
-    assert len(out) == 3
+    assert len(members) == 3
+
+
+def test_mongod_has_root_user(host):
+
+    def str2bool(v):
+        return str(v).lower() in ("yes", "true", "t", "1")
+
+    is_master = host.check_output(
+        "echo 'rs.isMaster().ismaster' | mongo --quiet")
+
+    raise TypeError(
+        'is_master: {}, type: {}'.format(is_master, type(is_master)) +
+        'bool: {}, type: {}'.format(bool(is_master), type(bool(is_master))) +
+        'b: {}, type: {}'.format(str2bool(is_master),
+                                 type(str2bool(is_master)))
+    )
+
+    if is_master:
+        has_admin = json.loads(
+            host.check_output(
+                "echo 'db.system.users.find({user: \"admin\"}).count()' " +
+                "| mongo --quiet admin"))
+        assert has_admin == 1
+
+
+# def test_mongod_keyfile(host):
+#     f = host.file('/var/lib/mongo/security.key')
+#     assert f.exists
+#     assert f.user == 'mongod'
+#     assert f.group == 'mongod'
+#     assert oct(f.mode) == '0600'
