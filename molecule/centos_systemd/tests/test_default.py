f3be1727b30df2afb56a9a7b5d704e6131af6f9c
jamatute <jamatute@paradigmadigital.com>
2017-09-06 15:34:59 +0200

    * [feat] Add keyfile

--- molecule/centos_systemd/tests/test_default.py
+++ molecule/centos_systemd/tests/test_default.py
@@ -20,7 +20,7 @@ def test_mongod_conf(host):
     assert f.user == 'root'
     assert f.group == 'root'
     assert oct(f.mode) == '0644'
-    # assert f.contains('keyFile:')
+    assert f.contains('keyFile:')
 
 
 def test_mongod_can_start(host):
@@ -33,7 +33,7 @@ def test_mongod_has_replicaset(host):
     members = json.loads(
         host.check_output(
             "echo 'rs.status().members.map(function(a) {return a.name});'" +
-            "| mongo --quiet"))
+            "| mongo --quiet -u admin -p nimda admin"))
     assert len(members) == 3
 
 
@@ -42,27 +42,27 @@ def test_mongod_has_root_user(host):
     def str2bool(v):
         return str(v).lower() in ("yes", "true", "t", "1")
 
-    is_master = host.check_output(
-        "echo 'rs.isMaster().ismaster' | mongo --quiet")
-
-    raise TypeError(
-        'is_master: {}, type: {}'.format(is_master, type(is_master)) +
-        'bool: {}, type: {}'.format(bool(is_master), type(bool(is_master))) +
-        'b: {}, type: {}'.format(str2bool(is_master),
-                                 type(str2bool(is_master)))
-    )
+    is_master = str2bool(host.check_output(
+        "echo 'rs.isMaster().ismaster' | mongo --quiet"))
 
     if is_master:
         has_admin = json.loads(
             host.check_output(
                 "echo 'db.system.users.find({user: \"admin\"}).count()' " +
-                "| mongo --quiet admin"))
+                "| mongo --quiet -u admin -p nimda admin"))
         assert has_admin == 1
 
 
-# def test_mongod_keyfile(host):
-#     f = host.file('/var/lib/mongo/security.key')
-#     assert f.exists
-#     assert f.user == 'mongod'
-#     assert f.group == 'mongod'
-#     assert oct(f.mode) == '0600'
+def test_mongod_keyfile(host):
+    f = host.file('/var/lib/mongo/security.key')
+    assert f.exists
+    assert f.user == 'mongod'
+    assert f.group == 'mongod'
+    assert oct(f.mode) == '0600'
+
+
+def test_template_files_doesnt_exist(host):
+    templates = ['/tmp/create_root.js', '/tmp/replicaset.js']
+    for filename in templates:
+        f = host.file(filename)
+        assert not f.exists
